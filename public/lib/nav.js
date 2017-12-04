b2e8d5181382e8960de159606068986252c52399
andres <andres.root.coder@gmail.com>
2017-12-04 01:04:59 -0500

    Setting up packages

--- public/lib/nav.js
+++ public/lib/nav.js
@@ -2,6 +2,7 @@ var Nav = function(className) {
 	this.element = document.querySelector(className);
 	this.logo = this.element.querySelector('.logo');
 	this.menu = this.element.querySelector('.menu');
+	this.icon = this.element.querySelector('.svg');
 };
 
 Nav.prototype.init = function init() {
@@ -14,9 +15,14 @@ Nav.prototype.init = function init() {
 };
 
 Nav.prototype.toggle = function toggle() {
+	// Push from left
 	this.logo.classList.toggle('toggle-left');
 	this.menu.classList.toggle('toggle-left');
 	document.querySelector('.content').classList.toggle('toggle-left');
+
+	// Change toggle icon
+	this.icon.classList.toggle('open');
+
 };
 
 topnav = new Nav('.hg-nav');
